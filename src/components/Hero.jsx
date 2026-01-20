d0473a3c05de412722c096cd672d64f3e0e41adf
Nethmi Diwyanga Nanayakkara <nethminanayakkara277@gmail.com>
2026-01-21 00:18:11 +0530

    Add Instagram logo asset to the project

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,80 @@
+import { button } from "framer-motion/client";
+import instagram from "../assets/Instagram.png"
+import facebook from "../assets/facebook.png"
+import github from "../assets/github.png"
 
+const Hero = ( darkMode ) => {
+    const socialIcons = [
+        {icon: instagram, alt: 'Instagram'},
+        {icon: facebook, alt: 'Facebook'},
+        {icon: github, alt: 'Github'},
 
-const Hero = () => {
+    ];
+
+    const darkTheme = {
+        textPrimary: 'text-white',
+        textSecondary: 'text-gray-300',
+        buttonSecondary: `text-white border-2 border-orange-500 
+        hover:bg-orange-600`,
+        decorativeCircle: 'bg-orange-500 opacity-10',
+    };
+
+    const lightTheme = {
+        textPrimary: 'text-gray-900',
+        textSecondary: 'text-gray-700',
+        buttonSecondary: `text-gray-800 border-2 border-orange-500 
+        hover:bg-orange-600 hover:text-white`,
+        decorativeCircle: 'bg-orange-400 opacity-20',
+
+    };
+
+    const theme = darkMode ? darkTheme : lightTheme;
   return (
-    <div>
+    <div className='relative overflow-hidden min-h-screen flex flex-col'>
+        <section
+        id='home'
+        data-aos = 'fade-up'
+        data-aos-delat = '250'
+        className='body-font z-10'>
+            <div className= 'container mx-auto flex px-4 sm:px-8 lg:px-14 py-12 lg:py-32 flex-col lg:flex-row items-center justify-between lg:mt-0 mt-14'>
+                <div className='lg:w-1/2 w-full flex flex-col items-center lg:items-start text-center lg:text-left mb-12 lg:mb-0'>
+                <div className= 'flex justify-center lg:justify-start gap-4 sm:gap-6 mb-6 sm:mb-7 w-full'>
+                    {socialIcons.map((social, index) => (
+                        <a 
+                        key = {index}
+                        href="#"
+                        target="_blank"
+                        data-aos-delay = {`${400 + index * 100}`}
+                        className="transform hover:scale-110 transition-transform duration-300">
+                            <img 
+                            src= {social.icon}
+                            alt= {social.alt}
+                            className= {`w-8 h-8 sm:w-10 sm:h-10
+                                object-contain ${darkMode
+                                    ? ''
+                                    : 'filter brightness-75'}`}
+                            >
+
+                            </img>
+
+                        </a>
+                    ))}
+
+                </div>
+                <h1 className={`title-font text-3xl sm:text-4xl
+                    lg:text-5xl mb-4 font-bold ${theme.textPrimary}`}
+                    data-aos = 'fade-up'
+                    data-aos-delay = '500'>
+                        Hi, I'm Nethmi Nanayakkara
+                </h1>
+                <p>
+                  I am a passionate Full-Stack Developer
+                  driven by a desire to create impactful
+                  and user-centric digital experiences.  
+                </p>
+                </div>
+            </div>
+        </section>
       
     </div>
   )
